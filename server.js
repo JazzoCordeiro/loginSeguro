7c7447282042f90792230fcd7031f2a73b1839c7
jazzocordeiro <cordeirorosadigital@gmail.com>
2024-10-27 07:45:59 -0300

    estudos

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { PrismaClient } from '@prisma/client';
+import cors from 'cors'
 
 const app = express();
 const prisma = new PrismaClient();
-
+app.use(cors());
 app.use(express.json());
 
 // Criar usuário
